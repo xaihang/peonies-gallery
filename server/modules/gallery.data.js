99aa9875c879030c93993247bb086ce73be6e41b
Chad Smith <chad@codingbychad.com>
2021-03-24 15:14:47 -0500

    added photos

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,21 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: '/images/venus_mars_jupiter.png', description: `The evening view from my home in Colorado. Venus is the bright star in the middle of the photo. Mars is the lower bright star to the left (10 o'clock position). Jupiter is the bright star just above Mars.`, likes: 4 },
+    { id: 3, path: 'images/firey_sunset.jpg', description: `A beautiful sunset taken from my porch at my house in Colorado.`, likes: 2 },
+    { id: 4, path: 'images/max.jpg', description: 'My first dog, Max.', likes: 0 },
+    { id: 5, path: 'images/saint_chappell.jpg', description: 'Interior and Rose of Sain Chappell in Paris, France.', likes: 0 },
+    { id: 6, path: 'images/park_near_louvre.jpg', description: 'A park just outside of the Musee Louvre, Paris, France.', likes: 1 },
+    { id: 7, path: 'images/versailles_ceiling.jpg', description: 'One of the many ornate ceilings of Versailles.', likes: 0 },
+    { id: 8, path: 'images/thomas_jefferson.jpg', description: 'Statue of Thomas Jefferson outside of the Musee Monmart, Paris, France.', likes: 0 },
+    { id: 9, path: 'images/rodin_three_men.jpg', description: 'Rodin sculpture, Musee Rodin, Paris, France.', likes: 0 },
+    { id: 10, path: 'images/rodin_woman.jpg', description: 'Rodin sculpture, Musee Rodin, Paris, France.', likes: 0 },
+    { id: 11, path: 'images/royal_cottages_versailles.jpg', description: `"Cottages" built for the Queen of France and her three closest friends. Versailles, France.`, likes: 2 },
+    { id: 12, path: 'images/gazebo.jpg', description: 'Marble gazebo in the gardens of Versailles, France.', likes: 3 },
+    { }
+
+
+
+
 ];
 
 module.exports = galleryItems;
