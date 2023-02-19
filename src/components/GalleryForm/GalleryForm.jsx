80b9a713ea679b08d28b8fe50b29204a520603f0
Xai Hang <xaihang12@gmail.com>
2023-02-18 18:07:09 -0600

    fixed errors and bugs

--- src/components/GalleryForm/GalleryForm.jsx
+++ src/components/GalleryForm/GalleryForm.jsx
@@ -1,43 +1,42 @@
-import { useState } from "react";
-import Input from "../Input/Input";
-import SubmitButton from "../SubmitButton";
-import "./GalleryForm.css";
+import { useState } from 'react';
+import Input from '../Input/Input';
+import SubmitButton from '../SubmitButton';
+import './GalleryForm.css';
 
 function GalleryForm({ addImage }) {
-    const [url, setUrl] = useState("");
-    const [description, setDescription] = useState("");
+  const [url, setUrl] = useState('');
+  const [description, setDescription] = useState('');
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        if (url && description) {
-            addImage({url,description});
-            setUrl("");
-            setDescription("");
-        } else {
-            alert("Do not leave input fields blank!");
-        }
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (url && description) {
+      addImage({ url, description });
+      setUrl('');
+      setDescription('');
+    } else {
+      alert('Do not leave input fields blank!');
+    }
+  };
 
-
-    return (
-            <form className="gallery-form">
-                <div id="input-container">
-                    <Input
-                        name="url"
-                        value={url}
-                        setValue={setUrl}
-                        placeHolder="image url"
-                    />
-                    <Input
-                        name="description"
-                        value={description}
-                        setValue={setDescription}
-                        placeHolder="image description"
-                    />
-                    <SubmitButton handleSubmit={handleSubmit} />
-                </div>
-            </form>
-    );
+  return (
+    <form className="gallery-form">
+      <div id="input-container">
+        <Input
+          name="url"
+          value={url}
+          setValue={setUrl}
+          placeHolder="image url"
+        />
+        <Input
+          name="description"
+          value={description}
+          setValue={setDescription}
+          placeHolder="image description"
+        />
+        <SubmitButton handleSubmit={handleSubmit} />
+      </div>
+    </form>
+  );
 }
 
 export default GalleryForm;
